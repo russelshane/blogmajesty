abc9d2ac4735abbb8ae8aca29e71bac69b289743
russelshane <hello@darsler.com>
2024-07-05 18:54:56 +0800

    project cleanup

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "BlogMajesty.",
+  description:
+    "Connecting minds and distributing voices in the Philippines for growth, technology, finance and travel.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
